8b0e8fd70b37e837fdf38717fbb72721e83c9687
Olufemi Salaam <salaam.olufemi@gmail.com>
2018-03-03 20:35:26 -0500

    text test

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "DORYASHAAAAAA";
 
   options = {
     hostname: 'api.groupme.com',
